fb55fa61ccbd9dc7aa248a04eef4d00fef496af0
Fabioes <fabionike@icloud.com>
2023-10-23 17:21:08 +0200

    feat / tempurata

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ let AnnoAttuale = 2023;
 let calcolo = AnnoAttuale - AnnoDiNAscita;
 alert(calcolo) */
 
+let Temperatura = prompt("indica temperatura in far")
+let Celcius1 = 32;
+let celcius2  = 9;
+let celcius3 = 5;
+let calcolo1 =  Temperatura - Celcius1;
+let calcolo2 =  calcolo1 / celcius2
+let calcolo3 = calcolo2 *  celcius3
+let frase = " ° celsius"
+alert(calcolo3 + frase) 
+
+
