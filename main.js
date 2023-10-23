67ca14c0ba628067720df755dbcc51f2c081bc3e
Fabioes <fabionike@icloud.com>
2023-10-23 17:01:48 +0200

    feat / la tua eta

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-
-
-let tazzeVendute = prompt("quante tazze hai venduto oggi?");
+/* let tazzeVendute = prompt("quante tazze hai venduto oggi?");
 let prezzo = 2;
 let guadagni = tazzeVendute * prezzo;
 let messaggio = "Oggi hai venduto " ;
 let fine = " tazze di caffe"
 let frase = messaggio + guadagni + fine;
-alert(frase);
+alert(frase); */
+
+/* let AnnoDiNAscita = prompt( "indica il tuo anno di nascita?")
+let AnnoAttuale = 2023;
+let calcolo = AnnoAttuale - AnnoDiNAscita;
+alert(calcolo) */
+
